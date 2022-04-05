754f361199cd1fce8c123d3fb96ac02598427410
Nikhil Subhash Jugale <52400252+nikhiljugale007@users.noreply.github.com>
2022-04-05 17:42:00 +0530

    added watchlater and liked video page (#4)

--- src/pages/liked-videos/LikedVideos.jsx
+++ src/pages/liked-videos/LikedVideos.jsx
@@ -1,5 +1,35 @@
+import { useEffect } from "react";
+import { getLikedVideos } from "../../api/apicalls";
+import { useVideoContext } from "../../context/VideoContext";
+import { VideoCard } from "../../components";
 const LikedVideos = () => {
-	return <div>LikedVideos</div>;
+	const { videoState, videoDispatch } = useVideoContext();
+
+	useEffect(() => {
+		const getLikedVideo = async () => {
+			const response = await getLikedVideos();
+			if (response.success) {
+				videoDispatch({
+					type: "SET_LIKED_VIDEOS",
+					payload: response.likes,
+				});
+			} else {
+				console.log("ERR");
+			}
+		};
+		getLikedVideo();
+	}, [videoDispatch]);
+	return (
+		<div>
+			{videoState.likedvideos.length < 1 && <h1>No liked videos </h1>}
+
+			<div className="grid  grid-4-responsive">
+				{videoState.likedvideos.map((video) => {
+					return <VideoCard key={video._id} video={video} />;
+				})}
+			</div>
+		</div>
+	);
 };
 
 export { LikedVideos };
--- src/pages/watch-later/WatchLater.jsx
+++ src/pages/watch-later/WatchLater.jsx
@@ -1,7 +1,32 @@
 import "./WatchLater.css";
-
+import { useEffect } from "react";
+import { getWatchLater } from "../../api/apicalls";
+import { useVideoContext } from "../../context/VideoContext";
+import { VideoCard } from "../../components";
 const WatchLater = () => {
-	return <div>WatchLater</div>;
+	const { videoState, videoDispatch } = useVideoContext();
+
+	useEffect(() => {
+		const getWatchLaterVideos = async () => {
+			const response = await getWatchLater();
+			if (response.success) {
+				videoDispatch({ type: "SET_WATCHLATER", payload: response.watchlater });
+			} else {
+				console.log("ERR");
+			}
+		};
+		getWatchLaterVideos();
+	}, [videoDispatch]);
+	return (
+		<div>
+			{videoState.watchlater.length < 1 && <h1>No videos in watch later </h1>}
+			<div className="grid  grid-4-responsive">
+				{videoState.watchlater.map((video) => {
+					return <VideoCard key={video._id} video={video} />;
+				})}
+			</div>
+		</div>
+	);
 };
 
 export { WatchLater };
