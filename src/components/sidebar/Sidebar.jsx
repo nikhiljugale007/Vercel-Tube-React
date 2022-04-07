a237c94cd5747dadac34faa39d9cd75ee1e77ed7
Nikhil Jugale <nikhiljugale007@gmail.com>
2022-04-07 21:15:37 +0530

    fixed active link issue.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,55 +7,60 @@ import {
 	FaThList,
 	FaThumbsUp,
 } from "../../icons";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Sidebar = ({ mobileSidebar, setMobileSidebar }) => {
-	const getActiveStyle = ({ isActive }) => ({
-		color: isActive ? "white" : "",
-	});
+	const location = useLocation().pathname;
+
 	return (
 		<>
 			{/* desktop sidebar */}
 			<div className="sidebar-container">
 				<NavLink
 					to="/"
-					className="sidebar-item link-no-style "
-					style={getActiveStyle}
+					className={`sidebar-item link-no-style + ${
+						location === "/" ? "active" : ""
+					}`}
 				>
 					<FaHome size={20} /> <p>Home</p>
 				</NavLink>
 				<NavLink
 					to="/videos"
-					className="sidebar-item link-no-style"
-					style={getActiveStyle}
+					className={`sidebar-item link-no-style + ${
+						location === "/videos" ? "active" : ""
+					}`}
 				>
 					<FaCompass size={20} /> <p>Explore</p>
 				</NavLink>
 				<NavLink
 					to="/playlist"
-					className="sidebar-item link-no-style "
-					style={getActiveStyle}
+					className={`sidebar-item link-no-style + ${
+						location === "/playlist" ? "active" : ""
+					}`}
 				>
 					<FaThList size={20} /> <p>Playlist</p>
 				</NavLink>
 				<NavLink
 					to="/liked-videos"
-					className="sidebar-item link-no-style "
-					style={getActiveStyle}
+					className={`sidebar-item link-no-style + ${
+						location === "/liked-videos" ? "active" : ""
+					}`}
 				>
 					<FaThumbsUp size={20} /> <p>Liked Videos</p>
 				</NavLink>
 				<NavLink
 					to="/watch-later"
-					className="sidebar-item link-no-style "
-					style={getActiveStyle}
+					className={`sidebar-item link-no-style + ${
+						location === "/watch-later" ? "active" : ""
+					}`}
 				>
 					<FaClock size={20} /> <p>Watch Later</p>
 				</NavLink>
 				<NavLink
 					to="/history"
-					className="sidebar-item link-no-style "
-					style={getActiveStyle}
+					className={`sidebar-item link-no-style + ${
+						location === "/history" ? "active" : ""
+					}`}
 				>
 					<FaHistory size={20} /> <p>History</p>
 				</NavLink>
@@ -65,49 +70,49 @@ const Sidebar = ({ mobileSidebar, setMobileSidebar }) => {
 				<div className="mobile-sidebar-container">
 					<NavLink
 						to="/"
-						className="sidebar-item link-no-style "
-						style={getActiveStyle}
-						onClick={() => setMobileSidebar((prev) => !prev)}
+						className={`sidebar-item link-no-style + ${
+							location === "/" ? "active" : ""
+						}`}
 					>
 						<FaHome size={20} /> <p>Home</p>
 					</NavLink>
 					<NavLink
 						to="/videos"
-						className="sidebar-item link-no-style"
-						style={getActiveStyle}
-						onClick={() => setMobileSidebar((prev) => !prev)}
+						className={`sidebar-item link-no-style + ${
+							location === "/videos" ? "active" : ""
+						}`}
 					>
 						<FaCompass size={20} /> <p>Explore</p>
 					</NavLink>
 					<NavLink
 						to="/playlist"
-						className="sidebar-item link-no-style "
-						style={getActiveStyle}
-						onClick={() => setMobileSidebar((prev) => !prev)}
+						className={`sidebar-item link-no-style + ${
+							location === "/playlist" ? "active" : ""
+						}`}
 					>
 						<FaThList size={20} /> <p>Playlist</p>
 					</NavLink>
 					<NavLink
 						to="/liked-videos"
-						className="sidebar-item link-no-style "
-						style={getActiveStyle}
-						onClick={() => setMobileSidebar((prev) => !prev)}
+						className={`sidebar-item link-no-style + ${
+							location === "/liked-videos" ? "active" : ""
+						}`}
 					>
 						<FaThumbsUp size={20} /> <p>Liked Videos</p>
 					</NavLink>
 					<NavLink
 						to="/watch-later"
-						className="sidebar-item link-no-style "
-						style={getActiveStyle}
-						onClick={() => setMobileSidebar((prev) => !prev)}
+						className={`sidebar-item link-no-style + ${
+							location === "/watch-later" ? "active" : ""
+						}`}
 					>
 						<FaClock size={20} /> <p>Watch Later</p>
 					</NavLink>
 					<NavLink
 						to="/history"
-						className="sidebar-item link-no-style "
-						style={getActiveStyle}
-						onClick={() => setMobileSidebar((prev) => !prev)}
+						className={`sidebar-item link-no-style + ${
+							location === "/history" ? "active" : ""
+						}`}
 					>
 						<FaHistory size={20} /> <p>History</p>
 					</NavLink>
