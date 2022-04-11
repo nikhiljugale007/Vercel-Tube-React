173bd546e802722d40ce82d33679b891a90af9ad
Nikhil Jugale <nikhiljugale007@gmail.com>
2022-04-11 14:41:40 +0530

    added comments

--- src/context/VideoFilterFunction.js
+++ src/context/VideoFilterFunction.js
@@ -1,9 +1,11 @@
+//catergory wise filtered data
 const getFilteredVideos = (videos, filters) => {
 	return filters.categoryName === "All"
 		? videos
 		: videos.filter((video) => video.categoryName === filters.categoryName);
 };
 
+//search wise filtered data
 const searchFilteredData = (videos, filters) => {
 	const typeFilterData = getFilteredVideos(videos, filters);
 	const searchData = typeFilterData.filter((video) => {
